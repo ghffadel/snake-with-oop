1dde83d264a32bb6b642d79f26aca876105fa0bf
Laís Dib Albuquerque <lda.snf20@uea.edu.br>
2021-06-11 15:42:18 -0400

    Adding constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+class Constants:
+    WIDTH = 800
+    HEIGHT = 600
+
+    FRAMERATE = 40
+
+    COLORS = {
+        "black": (0, 0, 0),
+        "white": (255, 255, 255),
+        "green": (0, 125, 0),
+        "red": (125, 0, 0)
+    }
