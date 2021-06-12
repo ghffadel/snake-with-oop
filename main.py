8fa95501353638200c7a6cca49d96b59c062a5bd
Leandro Santiago da Silva <lsds.snf20@uea.edu.br>
2021-06-12 00:25:49 -0400

    drawing snake and apple

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from window import Window
 from constants import Constants
+from snake import Snake
+from cube import Cube
 import pygame
 
 
 def main():
     window = Window()
     clock = pygame.time.Clock()
+    s = Snake((Constants.ROWS // 2, Constants.ROWS // 3))
 
     flag = True
+
     while flag:
         pygame.time.delay(Constants.FRAMERATE)
         clock.tick(10)
         window.draw_window()
+        for cube in s.cubes:
+            window.draw_cube(cube)
+
+        window.draw_cube(window.fruit)
+
+        pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- window.py
+++ window.py
@@ -8,7 +8,7 @@ import pygame
 class Window:
 	def __init__(self):
 		self.snake = Snake([Constants.WIDTH // 2, Constants.HEIGHT // 2])
-		self.fruit = Cube(Constants.COLORS["green"], [randint(10, Constants.WIDTH - 10), randint(10, Constants.HEIGHT - 10)])
+		self.fruit = Cube(Constants.COLORS["red"], [randint(1, Constants.ROWS - 1), randint(1, Constants.ROWS - 1)])
 		self.window = pygame.display.set_mode((Constants.WIDTH, Constants.HEIGHT))
 
 	def draw_window(self):
@@ -19,8 +19,6 @@ class Window:
 			y += Constants.WIDTH // Constants.ROWS
 			pygame.draw.line(self.window, Constants.COLORS["white"], (x, 0), (x, Constants.HEIGHT))
 			pygame.draw.line(self.window, Constants.COLORS["white"], (0, y), (Constants.WIDTH, y))
-
-		pygame.display.update()
 
 	def draw_cube(self, cube):
 		distance = Constants.WIDTH // Constants.ROWS
